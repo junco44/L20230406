3cee59b518dfa3300b5fb6daa34c2aa511198340
junco44 <cjswnsgh44@gmail.com>
2023-04-06 11:12:42 +0900

    Player, Monster Class 생성

--- main.cpp
+++ main.cpp
@@ -2,70 +2,7 @@
 
 using namespace std;
 
-class Player
-{
-private:
-	string PlayerName;
-	int HP;
-	int MP;
-
-public:
-	Player();
-	Player(string PlayerName, int HP, int MP);
-	void Move();
-	void Attack();
-};
-void Player::Move()
-{
-	
-}
-void Player::Attack()
-{
-	
-}
-
-class Monster
-{
-private:
-	string MonsterName;
-	int HP;
-
-public:
-	Monster();
-	Monster(string MonsterName, int HP);
-	void Move();
-	void Attack();
-};
-void Monster::Move()
-{
-
-}
-void Monster::Attack()
-{
-
-}
-
-class Map
-{
-private:
-	const int SizeX = 10;
-	const int SizeY = 10;
-
-public:
-	Map();
-	void Display();
-};
-Map::Map()
-{
-
-}
-void Map::Display()
-{
-
-}
-
 int main()
 {
-	
 	return 0;
 }
