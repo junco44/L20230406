cfae2c276c227699d97f466f9c141274f86b3635
junco44 <cjswnsgh44@gmail.com>
2023-04-06 10:19:18 +0900

    플레이어 추가

---
+++ main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+class Player
+{
+private:
+	string PlayerName;
+	int HP;
+	int MP;
+
+public:
+	Player();
+	Player(string PlayerName, int HP, int MP);
+	void Move();
+	void Attack();
+};
+void Player::Move()
+{
+	
+}
+void Player::Attack()
+{
+	
+}
+
+
+
+
+int main()
+{
+	
+	return 0;
+}
